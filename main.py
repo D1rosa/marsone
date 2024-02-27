8b9221d62cd000b0b9d78fe2a1398e8e7817ff72
D1rosa <151547045+D1rosa@users.noreply.github.com>
2024-02-28 01:28:25 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from flask import Flask, render_template, redirect, request, make_response, session, abort
+from data import session
+from data.users import User
+from data.jobs import Jobs
+# from forms.news import NewsForm
+# from forms.user import RegisterForm, LoginForm
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    session.global_init("db/blogs.db")
+    # app.run()
+    user = User()
+    user.name = "Ridley"
+    user.about = "Scott"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+    db_sess = session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
